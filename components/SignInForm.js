3b16726caeaf004d6b92779d39d23406fb7a3ed6
wveit <wilbertveit@rocketmail.com>
2021-02-21 02:10:35 -0800

    SignInForm hooked up to /users/login api

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -7,20 +7,44 @@ const defaultFields = {
 
 export function SignInForm({ onCancel, onSuccessfulSignIn }) {
     const [fields, setFields] = useState(defaultFields);
+    const [errors, setErrors] = useState(null);
 
     function handleChange(event) {
         const { name, value } = event.target;
         setFields({ ...fields, [name]: value });
     }
 
-    function handleSubmit() {
-        /* TODO: actually make api call to register */
-        const fakeUser = {
-            ...fields,
-            username: fields.email.split('@')[0],
-            token: 'lksdjflsdk',
-        };
-        onSuccessfulSignIn(fakeUser);
+    async function handleSubmit() {
+        let response = await fetch('/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(fields),
+        });
+        let responseObj = await response.json();
+        if (responseObj.errors) {
+            setErrors(responseObj.errors);
+            return;
+        }
+        const { token } = responseObj;
+
+        response = await fetch('/api/users/me', {
+            headers: {
+                'x-auth-token': token,
+            },
+        });
+        responseObj = await response.json();
+        if (responseObj.errors) {
+            setErrors({ meEndpoint: '/api/users/me failed' });
+            return;
+        }
+
+        const { username, email } = responseObj;
+
+        const user = { username, email, token };
+
+        onSuccessfulSignIn(user);
         onCancel();
     }
 
@@ -44,7 +68,13 @@ export function SignInForm({ onCancel, onSuccessfulSignIn }) {
             />
             <br />
             <button onClick={handleSubmit}>Submit</button>
-            <button onClick={onCancel}>Cancel</button>
+            <button onClick={onCancel}>Cancel</button>{' '}
+            {errors ? (
+                <div>
+                    <p>Errors:</p>
+                    <p>{JSON.stringify(errors)}</p>
+                </div>
+            ) : null}
         </div>
     );
 }
